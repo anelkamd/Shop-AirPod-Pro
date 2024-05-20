f85ef5a39ffe1d97485b47b04d8553aa8440a85a
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-05-20 20:09:25 +0100

    update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const ratio = .1;
-const option = {
+const options = {
     root : null,
     rootMargin : '0px',
     threshold : ratio
 }
-const hendlesInterst = function (entrie, observer) {
-    entrie.forEach( function (entry) {
+const hendlesInterst = function (entries, observer) {
+    entries.forEach( function (entry) {
         if (entry.entersectionRation > ratio) {
             entry.target.classList.add('.reveils-visible')
             observer.unobserve(entry.target)
@@ -14,4 +14,6 @@ const hendlesInterst = function (entrie, observer) {
 }
 
 const observer = new IntersectionObserver(hendlesInterst, option)
-observer.observe(document.querySelectorAll('.reveils'))
+document.querySelectorAll('.reveils').forEach( function (r) {
+    observer.observe(r)
+})
